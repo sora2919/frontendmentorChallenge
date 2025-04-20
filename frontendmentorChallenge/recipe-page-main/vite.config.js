2f05cabcb3655eee77bdf57faea0a36ccfd6af4a
sora <sora2919@gmail.com>
2025-04-20 15:53:08 +0800

    再次修改config設定

--- frontendmentorChallenge/recipe-page-main/vite.config.js
+++ frontendmentorChallenge/recipe-page-main/vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/frontendmentorChallenge/recipe-page-main/'
+  base: '/frontendmentorChallenge/frontendmentorChallenge/recipe-page-main-dist/'
 })
